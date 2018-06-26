d0af7573098cb3092acf3c72af0667e73b55bfbe
DESKTOP-U4BN2EH\Shafqat <shafqatali89@gmail.com>
2018-06-26 20:11:07 +0200

    Bootstrap Structure for header and footer

---
+++ structure.php
@@ -0,0 +1,20 @@
+
+<div class="container">
+    <div class="row text-center">
+        <div class="col-sm-6 col-xs-12 first-box">
+            <h2>
+                &nbsp;<a href="tel:+4943153647270">0431-53647270</a></h2>
+
+            <h2>
+                <a href="tel:+491742908220">0491742908220</a></h2>
+        </div>
+
+        <div class="col-sm-6 col-xs-12 second-box">
+            <h2>
+                &Ouml;ffnungszeiten</h2>
+
+            <h3>
+                Mo. - So.: 00.00 - 24.00 Uhr</h3>
+        </div>
+    </div>
+</div>
